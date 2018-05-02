aad4b4a5d7bd23354eaa8c789222eb998b07c029
gunvora <gunvor_a@telia.com>
2018-05-02 16:31:27 +0200

    With fetch from own API

--- code/src/components/app.js
+++ code/src/components/app.js
@@ -4,14 +4,35 @@ const productsJson = require("./../products.json")
 
 console.log("Products: (Remove this line in app.js!)", productsJson)
 
-
 class App extends React.Component {
-  
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      products: []
+    }
+  }
+
+  componentDidMount() {
+    this.fetchData()
+  }
+
+  fetchData = () => {
+    fetch("http://localhost:8080/products").then((response) => {
+      return response.json()
+    }).then((json) => {
+      console.log("here is data:", json)
+      this.setState({
+        products: json
+      })
+    })
+  }
+
   render() {
     return (
       <div className="App" >
 
-        {productsJson.products.map((product) => {
+        {this.state.products.map((product) => {
           return <Product
           key={product.id}
           name={product.name}
